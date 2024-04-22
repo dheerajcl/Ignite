28ecb1ded0a2fd61d877cee5281023ca53bf9fcb
dheerajcl <cldheeraj541@gmail.com>
2024-04-22 12:48:13 +0530

    fixing some config

---
+++ src/pages/_app.tsx
@@ -0,0 +1,42 @@
+import Navbar from "@/components/navbar";
+import { Toaster } from "@/components/ui/toaster";
+import { api } from "@/lib/api";
+import "@/styles/globals.css";
+import "@blocknote/react/style.css";
+import { type Session } from "next-auth";
+import { SessionProvider } from "next-auth/react";
+import { DefaultSeo } from "next-seo";
+import { type AppType } from "next/app";
+import { useRouter } from "next/router";
+import { SEO } from "../../next-seo.config";
+import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
+
+
+const MyApp: AppType<{ session: Session | null }> = ({
+  Component,
+  pageProps: { session, ...pageProps },
+}) => {
+  const router = useRouter();
+  const isReader = router.pathname.startsWith("/f/");
+  return (
+    <SessionProvider session={session}>
+      <DefaultSeo {...SEO} />
+      {isReader ? (
+        <Component {...pageProps} />
+      ) : (
+        <main>
+          <Navbar />
+          <div className="mx-auto flex flex-col">
+            <Component {...pageProps} />
+          </div>
+        </main>
+      )}
+      <Toaster />
+      <Analytics />
+      <SpeedInsights />
+    </SessionProvider>
+  );
+};
+
+export default api.withTRPC(MyApp);
